af3375cbd91efcb41acde5d3019b1f7f9ed99704
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2022-04-22 18:27:51 -0400

    set up db, along with creating functionality for user related functions

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import userController from './controllers/users-controller.js';
+
+const LOCAL_DB_URI = 'mongodb://localhost:27017/webdev'
+
+mongoose.connect(LOCAL_DB_URI);
 
 const app = express();
+
+app.use(cors({
+  credentials: true,
+  origin: 'http://localhost:3000'
+}));
+
+const sess = {
+  secret: 'keyboard cat',
+  cookie: {}
+};
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+app.use(session(sess));
+
 app.use(express.json());
-app.use(cors());
-app.get('/', (req, res) => {res.send('Hello!')})
-app.listen(process.env.PORT || 4000);
+userController(app);
+app.get('/', (req, res) => {res.send('Hello!')});
+app.listen(4000);
